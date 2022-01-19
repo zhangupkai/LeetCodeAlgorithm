262ec437d88d47baa75971889a30879e389c5b1c
zhangupkai <zhangshangkai@foxmail.com>
2022-01-19 21:42:05 +0800

    [binary]Solution0475Heaters update

--- src/binary/Solution0475Heaters.java
+++ src/binary/Solution0475Heaters.java
@@ -22,7 +22,7 @@ import java.util.Arrays;
 为了使供暖器可以覆盖所有房屋且供暖器的加热半径最小，对于每个房屋，应该选择离该房屋最近的供暖器覆盖该房屋，最近的供暖器和房屋的距离即为该房屋需要的供暖器的最小加热半径。
 所有房屋需要的供暖器的最小加热半径中的最大值即为可以覆盖所有房屋的最小加热半径。
 为了得到距离每个房屋最近的供暖器，可以将供暖器数组 heaters 排序，然后通过二分查找得到距离最近的供暖器。
-具体而言，对于每个房屋 house，需要在有序数组heaters 中找到最大的下标 i，使得 heaters[i]≤house，特别地，当 heaters[0]>house 时，i = -1。
+具体而言，对于每个房屋 house，需要在有序数组heaters 中找到最大的下标 i（最右二分），使得 heaters[i]≤house，特别地，当 heaters[0]>house 时，i = -1。
 在得到下标 i 之后，令 j = i + 1，则 j 是满足 heaters[j]>house 的最小下标。特别地，当 heaters[n−1]≤house 时，j = n，
 其中 n 是数组heaters 的长度。
 得到下标 i 和 j 之后，离房屋 house 最近的供暖器为heaters[i] 或 heaters[j]，分别计算这两个供暖器和房屋 house 的距离，
@@ -33,9 +33,9 @@ import java.util.Arrays;
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
-    //TODO 没完全搞懂
+// 本质上是最右二分搜索
 public class Solution0475Heaters {
-    public int findRadius(int[] houses, int[] heaters) {
+    public static int findRadius(int[] houses, int[] heaters) {
         int ans = 0;
         // 将供暖器数组 heaters 排序，然后通过二分查找得到距离最近的供暖器
         Arrays.sort(heaters);
@@ -48,13 +48,14 @@ public class Solution0475Heaters {
             int rightDistance = j >= heaters.length ? Integer.MAX_VALUE : heaters[j] - house;
             // 当前房屋所需的供暖器最小半径为左侧和右侧暖器中最小的
             int curDistance = Math.min(leftDistance, rightDistance);
+            System.out.println(curDistance);
             ans = Math.max(ans, curDistance);
         }
 
         return ans;
     }
 
-    public int binarySearch(int[] nums, int target) {
+    public static int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
 
@@ -63,18 +64,21 @@ public class Solution0475Heaters {
             return -1;
         }
 
-        // left = mid 导致 循环条件不能再是 left <= right
-        while (left < right) {
-            // TODO: + 1 的原因：
-            int mid = left + (right - left + 1) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
             }
             else {
-                // left = mid，而不是left = mid + 1，因为目标house位置可能没有heater，导致匹配不到
-                left = mid;
+                right = mid - 1;
             }
         }
-        return left;
+        return left - 1;
+    }
+
+    public static void main(String[] args) {
+        int[] houses = new int[]{1, 2, 3, 4};
+        int[] heaters = new int[]{1, 4};
+        System.out.println(findRadius(houses, heaters));
     }
 }
